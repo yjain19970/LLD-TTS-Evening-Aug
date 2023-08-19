0a566c13cd99b9b28757eaa37c4335b1d51f7e2b
yash.jain <yashjain19970@gmail.com>
2023-08-19 21:59:41 +0530

    adder subtractor

---
+++ src/main/java/addersubractor/Subtractor.java
@@ -0,0 +1,16 @@
+package addersubractor;
+
+public class Subtractor implements Runnable {
+    private Count count;
+
+    public Subtractor(Count count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; ++i) {
+            this.count.value -= i; // CRITICAL SECTION
+        }
+    }
+}
